fa580d36f5a3ff142b0d8ac146af4dbbedbeab9d
Mariya Moiseeva <mash.dobarina@gmail.com>
2023-06-05 14:59:04 +0300

    Исправление стилей для календаря

--- src/components/Calendar.jsx
+++ src/components/Calendar.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import styled from 'styled-components';
 import { Header } from "./Header";
-import { Button } from "./Button";
 import { Week } from "./Week";
 import { TimeTable } from "./TimeTable";
 
