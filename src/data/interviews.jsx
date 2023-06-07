a1a1ae8bb9c7cc96a7ef6128980b0e252458ee0d
Mariya Moiseeva <mash.dobarina@gmail.com>
2023-06-07 13:48:01 +0300

    Добавление данных

---
+++ src/data/interviews.jsx
@@ -0,0 +1,31 @@
+
+export const createNewArray = () => {
+    const arrayOfInterviewObject = [];
+    for(let i = 0; i < 12; i++) 
+        for(let j=0; j<7; j++) {
+            arrayOfInterviewObject[i][j] = Object.create(null, {
+                isInterview: false
+            })
+        }
+    return arrayOfInterviewObject
+}
+
+export const arrayOfInterviewObject = [
+    [{isInterview: true}, {isInterview: false}, {isInterview: false}, {isInterview: false}, {isInterview: false}, {isInterview: false}, {isInterview: false}],
+    [{isInterview: false}, {isInterview: false}, {isInterview: false}, {isInterview: false}, {isInterview: false}, {isInterview: false}, {isInterview: false}],
+    [{isInterview: false}, {isInterview: false}, {isInterview: false}, {isInterview: false}, {isInterview: false}, {isInterview: false}, {isInterview: false}],
+    [{isInterview: false}, {isInterview: false}, {isInterview: true}, {isInterview: false}, {isInterview: false}, {isInterview: false}, {isInterview: false}],
+    [{isInterview: false}, {isInterview: false}, {isInterview: false}, {isInterview: false}, {isInterview: false}, {isInterview: false}, {isInterview: false}],
+    [{isInterview: false}, {isInterview: true}, {isInterview: false}, {isInterview: false}, {isInterview: false}, {isInterview: false}, {isInterview: false}],
+    [{isInterview: false}, {isInterview: false}, {isInterview: false}, {isInterview: false}, {isInterview: false}, {isInterview: false}, {isInterview: false}],
+    [{isInterview: false}, {isInterview: false}, {isInterview: false}, {isInterview: false}, {isInterview: false}, {isInterview: false}, {isInterview: false}],
+    [{isInterview: false}, {isInterview: false}, {isInterview: false}, {isInterview: false}, {isInterview: false}, {isInterview: false}, {isInterview: false}],
+    [{isInterview: false}, {isInterview: false}, {isInterview: false}, {isInterview: false}, {isInterview: false}, {isInterview: false}, {isInterview: false}],
+    [{isInterview: false}, {isInterview: false}, {isInterview: false}, {isInterview: false}, {isInterview: false}, {isInterview: false}, {isInterview: false}],
+    [{isInterview: false}, {isInterview: false}, {isInterview: false}, {isInterview: false}, {isInterview: false}, {isInterview: false}, {isInterview: false}],
+    [{isInterview: false}, {isInterview: false}, {isInterview: false}, {isInterview: false}, {isInterview: false}, {isInterview: false}, {isInterview: false}],
+]
+
+
+
+
