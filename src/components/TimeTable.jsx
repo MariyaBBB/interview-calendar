788bc6f7f933517198ad48a497f752eca24997a6
Mariya Moiseeva <mash.dobarina@gmail.com>
2023-06-07 13:48:40 +0300

    Создала сетку событий

---
+++ src/components/TimeTable.jsx
@@ -0,0 +1,55 @@
+import React from "react";
+import styled from 'styled-components';
+import { arrayOfInterviewObject } from "../data/interviews";
+
+const StyledTimeTable = styled.div`
+    height: auto;
+    background: #FFFFFF;
+    display: grid;
+    grid-template-columns: 70px 1fr;
+    color: #C0C0C0;
+    overflow: hidden;
+    font-size: 20px;
+`
+
+const StyledInterview = styled.div`
+    grid-column: 2; 
+    display: grid;
+    grid-template-columns: repeat(7, auto);
+    text-align: center;
+    place-items: center;
+    border:none;
+`
+
+const StyledTime = styled.div`
+    padding-top: 28px;
+    padding-left: 14px;
+`
+
+const StyledObj = styled.div`
+    border-right: 2px solid #ECECEC;
+    border-bottom: 2px solid #ECECEC;
+    margin-top: -20px;
+    border-left: none;
+    border-top:none;
+    width: 100%;
+    height: 100%;   
+    ${props => props.$backgroundColor ? {background: '#EBECFF'}: '' };
+`
+
+export const TimeTable = (props) => {
+    
+    console.log(props.time.time)
+    return (
+        <> 
+            <StyledTimeTable {...props}>{props.time.time.map((t, index) => (
+                <>
+                <StyledTime>{t}</StyledTime>
+                <StyledInterview>{arrayOfInterviewObject[index].map(obj => <StyledObj $backgroundColor={obj.isInterview ? true : false}>{}</StyledObj>)}</StyledInterview>
+                </>
+                ))}
+            </StyledTimeTable>
+            
+        </>
+    )
+}
