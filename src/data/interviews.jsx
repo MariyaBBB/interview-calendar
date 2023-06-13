4df5716feca36cd8281a1eada6f5c3122ba7b469
Mariya Moiseeva <mash.dobarina@gmail.com>
2023-06-13 13:37:02 +0300

    Исправление данных событий

--- src/data/interviews.jsx
+++ src/data/interviews.jsx
@@ -1,7 +1,7 @@
 
 export const createNewArray = () => {
     const arrayOfInterview = [];
-    for(let i = 0; i < 12; i++) 
+    for(let i = 0; i < 13; i++) 
         for(let j=0; j<7; j++) {
             arrayOfInterview[i][j] = '';
         }
@@ -9,7 +9,7 @@ export const createNewArray = () => {
 }
 const array = new Array(7);
 for(let i = 0; i < 7; i++) {
-    array[i] = new Array(12);
+    array[i] = new Array(13);
 }
 
 export const arrayOfInterviews = array;
